adc488c9d890d3468fd65badd5428ad7b7eb030f
St Janne <jannis020609@gmail.com>
2020-09-16 13:54:53 +0200

    Added class and everything needed

---
+++ Modellering/Game.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Modellering
+{
+    public class Game
+    {
+        public string gun = "";
+        public string character = "";
+        public int healthKits = 0;
+    }
+}
--- Modellering/Program.cs
+++ Modellering/Program.cs
@@ -6,7 +6,25 @@ namespace Modellering
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Game kit1 = new Game() {gun = "Revolver", character = "Speed Warrior", healthKits = 2};
+            Game kit2 = new Game() {gun = "Sniper", character = "American Sniper", healthKits = 0};
+            
+
+            Game[] gameKits = {kit1, kit2};
+            System.Console.WriteLine("Choose a kit: ");
+            for (int i = 0; i < gameKits.Length; i++)
+            {
+                System.Console.WriteLine("\n" + gameKits[i].character);
+            }
+            string chosenKit = Console.ReadLine();
+            bool success = int.TryParse(chosenKit, out int chosenKitInt);
+            chosenKitInt -= 1;
+            System.Console.WriteLine("You chose: " + gameKits[chosenKitInt].character);
+            System.Console.WriteLine("Press Enter to continue");
+            Console.ReadLine();
+            Console.Clear();
+            System.Console.WriteLine($"Class information: \nClass name: {gameKits[chosenKitInt].character}\nGun: {gameKits[chosenKitInt].gun}\nAmount of healthpacks: {gameKits[chosenKitInt].healthKits}");
+            Console.ReadLine();
         }
     }
 }
